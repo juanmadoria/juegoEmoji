a917bb7634a7c9bf763f370b59bbcd0ab8bedd90
Juan Manuel Doria <juanmadoria98@gmail.com>
2022-02-16 15:53:18 -0500

    se agrego el emoji malo, reset y vida

--- juego.js
+++ juego.js
@@ -1,38 +1,59 @@
 const cuadro = document.querySelectorAll('.cuadro')
 const emoji = document.querySelector('.emoji')
+const emojiMalo = document.querySelector('.emojiMalo')
 const tiempo = document.querySelector('#tiempo')
 const puntos = document.querySelector('#puntos')
+const vidas = document.querySelector('#vida')
+
 
 let resultado = 0
 let lugarGolpe
+let lugarGolpeMalo
 let tiempoInicial = 60
 let idTiempo = null
+let oportunidades = 3
 
 
 //pintar el emoji y removerlo
-function cuadroRandom(){
+function cuadroRandom() {
     cuadro.forEach(cuadro => {
         cuadro.classList.remove('emoji')
+        cuadro.classList.remove('emojiMalo')
     })
-    let cuadroRandom = cuadro[Math.floor(Math.random() *9)]
+    let cuadroRandom = cuadro[Math.floor(Math.random() * 9)]
     cuadroRandom.classList.add('emoji')
-    lugarGolpe= cuadroRandom.id
+    let cuadroRandomMalo = cuadro[Math.floor(Math.random() * 9)]
+    cuadroRandomMalo.classList.add('emojiMalo')
+    lugarGolpe = cuadroRandom.id
+    lugarGolpeMalo = cuadroRandomMalo.id
 }
 
 //recorre el tablero escucha de mouse
-cuadro.forEach(cuadro =>{
-    cuadro.addEventListener('mousedown', () =>{
-        if (cuadro.id == lugarGolpe){
+cuadro.forEach(cuadro => {
+    puntos
+    puntos.textContent = resultado
+    oportunidades
+    vidas.textContent = oportunidades
+    cuadro.addEventListener('mousedown', () => {
+        if (cuadro.id == lugarGolpe) {
             resultado++
             puntos.textContent = resultado
-            lugarGolpe=null
+            lugarGolpe = null
+        }
+        if (cuadro.id == lugarGolpeMalo) {
+            /*resultado--
+            puntos.textContent = resultado
+            lugarGolpe = null*/
+            oportunidades--
+            alert('Solo te quedan ' + oportunidades + ' vida')
+            reinicio()
+            vidas.textContent = oportunidades
         }
     })
 })
 
-
 //intervalo de tiempo de ejecución
-function moverEmoji(){
+function moverEmoji() {
     idTiempo = setInterval(cuadroRandom, 500)
 }
 
@@ -40,11 +61,11 @@ function moverEmoji(){
 moverEmoji()
 
 //tiempo
-function contadorRegresivo(){
+function contadorRegresivo() {
     tiempoInicial--
     tiempo.textContent = tiempoInicial
 
-    if(tiempoInicial==0){
+    if (tiempoInicial == 0) {
         clearInterval(conteoTiempo)
         clearInterval(idTiempo)
         alert('Game Over Su puntaje es : ' + resultado);
@@ -52,9 +73,23 @@ function contadorRegresivo(){
 }
 
 //ejecuta la funcion que retrocede el tiempo
-let conteoTiempo = setInterval(contadorRegresivo,1000)
+let conteoTiempo = setInterval(contadorRegresivo, 1000)
 
+function resetJuego() {
+    alert('El juego se va a reiniciar..')
+    tiempoInicial = 60
+    tiempoInicial.textContent = tiempoInicial
+    resultado = 0
+    puntos.textContent = resultado
+    oportunidades = 3
+    vidas.textContent = oportunidades
 
+}
 
+function reinicio() {
+    tiempoInicial = 60
+    tiempoInicial.textContent = tiempoInicial
+    resultado = 0
+    puntos.textContent = resultado
 
-
+}
