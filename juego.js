f3e31f488f1a59bdad899f86b9c695b49eede9ab
Lucas Blandon <lucasblandon@gmail.com>
2022-02-16 08:30:31 -0500

    se agrega constantes a el juego.js

---
+++ juego.js
@@ -0,0 +1,5 @@
+const cuadro = document.querySelectorAll('.cuadro')
+const emoji = document.querySelector('.emoji')
+const tiempo = document.querySelector('#tiempo')
+const puntos = document.querySelector('#puntos')
+
